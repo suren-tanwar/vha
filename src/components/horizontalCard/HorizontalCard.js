c92e04cfb7a8cdade83e7c6b58284b55016dd7f9
Surendra Singh <surendra.zestbrains@gmail.com>
2022-05-04 15:12:57 +0530

    new

--- src/components/horizontalCard/HorizontalCard.js
+++ src/components/horizontalCard/HorizontalCard.js
@@ -1,7 +1,7 @@
-// // handleChange = (e) => {
-// //   console.log(e.target.value)
-// //   this.setState({ word: this.state.word1.filter((get)=> e.target.value == get.id) });
-// // };
+// handleChange = (e) => {
+//   console.log(e.target.value)
+//   this.setState({ word: this.state.word1.filter((get)=> e.target.value == get.id) });
+// };
 
 // import React, { Component } from 'react'
 // import './HorizontalCard.css'
@@ -121,7 +121,11 @@
 //                 width: 400,
 //                 fontStyle: "normal",
 //                 align: "left",
-//                 id:"1"
+//                 id:"1",
+//                 offsetY:0,
+//                 offsetX:0,
+//                 textYTextArea: 0,
+//                 textXTextArea: 0,
 //             },
 //             {
 //               textX: 0,
@@ -132,7 +136,11 @@
 //                 width: 400,
 //                 fontStyle: "normal",
 //                 align: "left",
-//                 id:"1"
+//                 id:"2",
+//                 offsetY:0,
+//                 offsetX:0,
+//                 textYTextArea: 0,
+//                 textXTextArea: 0,
 //             }
 //               ],
         
@@ -280,7 +288,6 @@
 //       //for localstorage data in to state data
 //       const localdata = await localStorage.getItem("stateSaved");
 //       this.setState({ newData: localdata })
-
 //       const state = await localStorage.getItem("stateSaved");
 //       if (state) this.setState(JSON.parse(state))
 //       else this.setState({ selectedObject: this.state.arrayObjectsLayer[0] })
@@ -754,6 +761,47 @@
 //   if (state) this.setState(JSON.parse(state));
 // };
 
+//   // TEXT EDITOR FUNCTIONALITY
+//   handleTextClick = (e, index) => {
+//     console.log('selecttext')
+//     const absPos = e.target.getAbsolutePosition();
+//     const stageBox = this.stageRef.current.container().getBoundingClientRect();
+//     let { rectangles } = this.state;
+//     rectangles[index].textXTextArea =(stageBox.left + absPos.x + this.containerCanvas.current.scrollLeft) / this.state.zoom;
+//     rectangles[index].textYTextArea =stageBox.bottom + absPos.y - stageBox.height + 40 + this.containerCanvas.current.scrollTop;
+//     this.setState({
+//       rectangles,
+//     });
+//   };
+//   selectText = (selectedId, index = undefined) => {
+//     console.log('selecttext')
+//     let { rectangles, indexTextSelected } = this.state;
+//    if (index) {
+//       indexTextSelected = index - 1;
+//    } else {
+//       if (rectangles[indexTextSelected]) {
+//         indexTextSelected = undefined;
+//       }
+//     }
+//     this.setState({
+//       selectedId,
+//       rectangles,
+//       indexTextSelected,
+//     });
+//   };
+
+//   setTextObject = rectangles => {
+//     this.setState({
+//      rectangles,
+//    });
+//  };
+//  handleTempEdit = (e, index) => {
+//   let { rectangles } = this.state;
+//   rectangles[index].textValue = e.target.value;
+//  this.setState({
+//     rectangles,
+//   });
+// };
 //   render() {
 //     console.log( this.state.rectangles)
 //     console.log(this.state)
@@ -1568,6 +1616,14 @@
 // {/**************************CANVAS */}
 // <div className="containerCanvas" ref={this.containerCanvas} >
 // <div className={`container-area`}>
+// <textarea
+// // key={index}
+// value={this.state.rectangles.textValue} 
+// onChange={e => this.handleTempEdit(e)}
+// type="text"
+// placeholder='Enter text' rows="6" cols="30"
+// className='text-field-text'
+// />
 // <Stage
 //   scaleY={1 / zoom}
 //   scaleX={1 / zoom}
@@ -1586,20 +1642,23 @@
 // >
 
 // <Layer>
-// {/****
-// {   this.state.rectangles && this.state.rectangles.map((rect, i) => {
+
+
+// {   this.state.rectangles && this.state.rectangles.map((rect, index) => {
 //   return (
 //    <Transformtext
-//       key={i}
+//       key={index}
+//        onSelect={() => {
+//         this.selectText(rect.id);
+//       }}
 //       shapeProps={rect}
 //       isSelected={rect.id === selectedId}
-//       onSelect={() => {
-//         this.selectShape(rect.id);
-//       }}
-      
-//       onChange={(newAttrs) => {
+//       handleTextDblClick={e =>
+//       this.handleTextClick(e, index)
+//         }
+//        onChange={(newAttrs) => {
 //         const rects = this.state.rectangles.slice();
-//         rects[i] = newAttrs;
+//         rects[index] = newAttrs;
 //         // this.ectangles(rects);
 //         this.setArrayObject(rects);
 //       }}
@@ -1607,7 +1666,7 @@
 //     />
 //   );
 // })}
-//  */}
+
 // {
 //   arrayObjectsLayer &&
 //   arrayObjectsLayer.map((item, index) => {
@@ -1808,151 +1867,106 @@
 
 // export default HorizontalCard
 
+// import React from 'react';
+// import { Group, Text } from 'react-konva';
 
-import React, { Component } from "react";
-import { Stage, Layer, Text,Transformer } from "react-konva";
+// import  TextEditor  from './text-editor';
+// import Konva from 'konva';
 
-export default class HorizontalCard extends Component {
-constructor(props) {
-  super(props)
-  this.shapeRef = React.createRef();
-  this.trRef = React.createRef();
-  this.state = {
-    isSelected:false,
-    selectedShapeName: "",
-      newTextObj: {
-      textX: 0,
-      fill: "black",
-      textY: 0,
-      textValue: "double click to edit and enter to finish",
-      fontSize: 8,
-      width: 400,
-      fontStyle: "normal",
-      align: "left",
-      id: 0
+//  const HorizontalCard = ({ text, onChange, ...props }) => {
+//   const [editorEnabled, setEditorEnabled] = React.useState(false);
+//   const textRef = React.useRef();
+//   // const textRef = React.useRef<Konva.Text>();
+
+//   return (
+
+//     <Group draggable>
+//       <Text
+//         text="helllllo"
+//         // text={text}
+//         ref={textRef}
+//         width={100}
+//         onClick={() => {
+//           setEditorEnabled(true);
+//         }}
+//         visible={!editorEnabled}
+//         {...props}
+//       />
+//       {editorEnabled && (
+//         <Group>
+//           <TextEditor
+//             value={text}
+//             // text="helllllo"
+//             textNodeRef={textRef}
+//             onChange={onChange}
+//             onBlur={() => {
+//               setEditorEnabled(false);
+//             }}
+//           />
+//         </Group>
+//       )}
+//     </Group>
+//   );
+// };
+// export default HorizontalCard
+
+import React, { Component } from 'react'
+import { Group, Stage, Text,Layer } from 'react-konva';
+// import  TextEditor  from './text-editor';
+import { Html } from 'react-konva-utils'
+
+export class HorizontalCard extends Component {
+  constructor(props) {
+    super(props)
+    this.textRef = React.createRef();
+    this.groupRef = React.createRef(null);
+    this.state = {
+      editorEnabled:false,
+      text:"helllllo"
     }
   }
-}
-componentDidMount(){
-  // if (this.state.isSelected === true && this.trRef.current !== null) {
-    if (this.state.isSelected === true) {
-    this.trRef.current.setNode(this.shapeRef.current);
-    this.trRef.current.getLayer().batchDraw();
-  }
- }
-
- handleEdit = e => {
-    console.log("click1")
-    let { newTextObj } = this.state;
-    newTextObj.textValue = e.target.value;
-    this.setState({
-      newTextObj
-    });
-  };
-
-  handleTextClick = e => {
-    console.log("click2")
-    const absPos = e.target.getAbsolutePosition();
-    console.log(absPos)
-    let { newTextObj } = this.state;
-    newTextObj.textX = absPos.x;
-    newTextObj.textY = absPos.y;
-    this.setState({
-      newTextObj,
-      isSelected:true
-    });
-  };
-  handleResize = (e) => {
-    console.log("click3")
-    if (this.shapeRef.current !== null) {
-      const textNode = this.shapeRef.current;;
-      const newWidth = textNode.width() * textNode.scaleX();
-      const newHeight = textNode.height() * textNode.scaleY();
-      textNode.setAttrs({
-        width: newWidth,
-        scaleX: 1
-      });
-  };
-
-  const shape = this.shapeRef.current;
-  // const shape = e.target;
-  console.log(shape)
-  shape.scaleX(1);
-  shape.scaleY(1);
-   this.props.onTransform({
-    x: shape.x(),
-    y: shape.y(),
-    width: shape.width() * shape.scaleX(),
-    height: shape.height() * shape.scaleY(),
-    rotation: shape.rotation()
- });
-}
-
-handleResize1 = (e) => {
-  console.log("click4")
+  onChange = (e) => {
   this.setState({
-    // isDragging: false,
-    x: e.target.x(),
-    y: e.target.y(),
+    [e.target.name]: e.target.value,
   });
-}
+};
   render() {
     console.log(this.state)
     return (
       <div>
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer>
-            <Text
-            ref={this.shapeRef}
-            // x={this.props.x}
-            // y={this.props.y}
-            width={150}
-            height={100}
-           scaleX={1}
-            scaleY={1}
-              fontSize={20}
-              align={"left"}
-              fontStyle={20}
-              draggable
-              text={this.state.newTextObj.textValue}
-              x={100}
-              y={100}
-              wrap="word"
-              // width={this.state.newTextObj.width}
-              onDblClick={e => this.handleTextClick(e)}
-              onClick={(e) => this.setState({isSelected:true})}
-              onTransformEnd={(e) => this.handleResize(e)}
-              onDragEnd={(e) => this.handleResize1(e) }
-              // onDragEnd={(e) => {
+     <Stage>
+     <Layer>
+     <Text
+     ref={this.textRef}
+     text={this.state.text}
+     width={100}
+     onClick={() => this.setState({editorEnabled : true})}
+    //  visible={!this.state.editorEnabled}
+   />
 
-              // }}
-            />
-               
-            {this.state.isSelected === true &&
-           <Transformer
-            ref={this.trRef}
-            rotateEnabled={false}
-            flipEnabled={false}
-            enabledAnchors={["middle-left", "middle-right"]}
-            boundBoxFunc={(oldBox, newBox) => {
-              newBox.width = Math.max(30, newBox.width);
-              return newBox;
-            }} />
-           }
-         </Layer>
-        </Stage>
-        <textarea
-          value={this.state.newTextObj.textValue}
-          style={{
-            position: "absolute",
-            top: 10,
-            left: 10,
-            color:"black"
-          }}
-          onChange={e => this.handleEdit(e)}
+     <Group ref={this.groupRef} draggable x={50} y={100}>
+      <Html divProps={{  }}>
+         <textarea className="polotno-input"
+            style={{position: "absolute",top:100,left:100,width: 100,height: 100,fontSize: 28,color: "black",  }}
+             type="text"
+             name='text'
+            value={this.state.text}
+          //  textNodeRef={this.textRef}
+           onChange={(e) => this.onChange(e)}
+           onBlur={() => this.setState({editorEnabled : false})}
+         />
+       </Html>
+       </Group>
+     
 
-        />
+   </Layer>
+   </Stage>
+
+      
+      
       </div>
-    );
+    )
   }
 }
+
+export default HorizontalCard
